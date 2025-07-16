0f612c810711e231276cc4c33c9120b5d173f52d
UmangSonika <sonika.1@iitj.ac.in>
2025-07-16 22:08:31 +0530

    Tanstack Query Setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,11 +5,17 @@ import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import chatRoutes from "./routes/chat.route.js";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";
 
 // dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 
 app.use(express.json());
 app.use(cookieParser());
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,8 +7,21 @@ import CallPage from "./pages/CallPage.jsx";
 import ChatPage from "./pages/ChatPage.jsx";
 import OnboardingPage from "./pages/OnboardingPage.jsx";
 import { Toaster } from "react-hot-toast";
+import { useQuery } from "@tanstack/react-query";
+import { axiosInstance } from "./lib/axios.js";
 
 const App = () => {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["todos"],
+    queryFn: async () => {
+      const res = await axiosInstance.get("/auth/me");
+      return res.data;
+    },
+    retry: false,
+  });
+
+  console.log(data);
+
   return (
     <div className="h-screen" data-theme="night">
       <Routes>
